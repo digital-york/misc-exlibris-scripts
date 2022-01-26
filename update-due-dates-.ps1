8337cef0fad955fb4312ef3279268bf8e55f8d0c
Paul Harding <paul.harding@york.ac.uk>
2022-01-26 09:11:18 +0000

    Update update-due-dates-.ps1
    Final version

--- update-due-dates-.ps1
+++ update-due-dates-.ps1
@@ -5,14 +5,10 @@
 [Reflection.Assembly]::LoadWithPartialName("System.Web") | Out-Null
 
 #define variables
-$url_prefix = "https://api-na.hosted.exlibrisgroup.com/"
-$queryParams = '?' +  [System.Web.HttpUtility]::UrlEncode('apikey') + '=' + [System.Web.HttpUtility]::UrlEncode('');
-
-#$api_key = [System.Web.HttpUtility]::UrlEncode($key) 
-$file = "C:\Work\overdue Lost Loans\test.xlsx"
+$url_prefix = "https://api-eu.hosted.exlibrisgroup.com/"
+$queryParams = '?' + [System.Web.HttpUtility]::UrlEncode('apikey') + '=' + [System.Web.HttpUtility]::UrlEncode('l8xx96250cee59df42d49afdfb41035812a7');
+$file = "C:\Work\overdue Lost Loans\overdue.xlsx"
 $sheetName = "overdue"
-$library = "EXTST"
-$circ_desk = "DEFAULT_CIRC_DESK"
 
 #Create an instance of Excel.Application and Open Excel file
 $objExcel = New-Object -ComObject Excel.Application
@@ -43,17 +39,16 @@ $rowusr,$coluser,$colid = 1,1,2
 			Exit
 		}
 		
-		Write-Host "Processing user" $user " row number" ($rowusr) "loan id " $loan_id
+		Write-Host "Processing user" $user "loan id " $loan_id
 		#retrieve item info
 		$loan_dets = $url_prefix + "almaws/v1/users/" + $user + "/loans/" + $loan_id + $queryParams
 		
-        #https://api-eu.hosted.exlibrisgroup.com/almaws/v1/users/cc1684/loans/42358939650001381?apikey=l8xx1f7d2163f0884b06b952e3f942e80b47
-
 
 		Write-Host "Loan Details URL " $loan_dets
 			
 	try{		
 			#API call and assign result to xml variable
+            #retrieve loan detail xml
 			[xml]$xml = Invoke-RestMethod -Method 'GET' -Uri $loan_dets	
 			#Start-Sleep 3
 		}
@@ -63,32 +58,33 @@ $rowusr,$coluser,$colid = 1,1,2
 		write-host "Fatal error: Get Loan Details" -ForegroundColor Red
 		write-host "Exception Type: $($_.Exception.GetType().FullName)" -ForegroundColor Red
 		write-host "Exception Message: $($_.Exception.Message)" -ForegroundColor Red	
-		Add-Content "C:\Work\Bulk returns\get-item-errors.txt" $barcode
+		Add-Content "C:\Work\overdue Lost Loans\get-item-errors.txt" $loan_id
 		}
-				
-		
+
 
 		$due_date = $xml.item_loan.due_date
         $mms_id = $xml.item_loan.mms_id
         $holding_id = $xml.item_loan.holding_id
         $item_id = $xml.item_loan.item_id
         $loan_id = $xml.item_loan.loan_id
-			
-		Write-Host "Due date is"  $due_date
-
-        $xml.item_loan.due_date = "2022-10-01T22:59:00Z"
+			       
+        #set new due date     
+        $xml.item_loan.due_date = '2022-02-07T23:59:00'
         
         $update_url = $url_prefix + "almaws/v1/users/" + $user + "/loans/" + $loan_id +  $queryParams
 
-        Write-Host $update_url
-
-
-        #update due date
-        
-        Invoke-RestMethod -Method 'PUT' -Uri $update_url -ContentType 'application/xml' -Body $xml
-      
-
-				
+        #update due date		
+	try{		
+			Invoke-RestMethod -Method 'PUT' -Uri $update_url -ContentType 'application/xml' -Body $xml
+			Start-Sleep 3
+		}
+	catch	
+		{
+		write-host "Fatal error: Update Due Date" -ForegroundColor Red
+		write-host "Exception Type: $($_.Exception.GetType().FullName)" -ForegroundColor Red
+		write-host "Exception Message: $($_.Exception.Message)" -ForegroundColor Red	
+		Add-Content "C:\Work\overdue Lost Loans\update-errors.txt" $loan_id
+		}				
 	}
 #close excel file
 $objExcel.quit()
